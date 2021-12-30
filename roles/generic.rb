8923b29430fe27d4224988384a80ac5193ed7310
SanjayaPanigrahi <sanjayp.panigrahi@gmail.com>
2021-12-30 13:51:12 +0530

    roles updated

--- roles/generic.rb
+++
@@ -1,4 +0,0 @@
-name "generic"
-description "Basic utility Instalation"
-run_list "recipe[install-base]"
-
--- roles/lb.rb
+++
@@ -1,4 +0,0 @@
-name "lb"
-description "Load Balancer Role - HaProxy Load Balancer"
-run_list "recipe[install-lb-haproxy]"
-
